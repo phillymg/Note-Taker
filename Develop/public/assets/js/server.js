b8ae26b8f2945cadcb1178d5b71be61d91bbfc13
Philmg <phillipmguetterman@att.net>
2023-08-30 19:07:26 -0500

    add html routes

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -6,6 +6,16 @@ const PORT = 3001;
 
 app.use(express.static('public'));
 
+app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
+
+app.get('/send', (req, res) =>
+    res.sendFile(path.join(__dirname, 'public/sendFile.html'))
+);
+
+app.get('/routes', (req, res) =>
+    res.sendFile(path.join(__dirname, 'public/routes.html'))
+);
+
 
 app.get('/notes', (req, res) => {
     const notesPath = path.join(__dirnam, '../notes.html');
@@ -13,5 +23,6 @@ app.get('/notes', (req, res) => {
 });
 
 app.listen(PORT, () =>
-    console.log(`App is listening on port http://localhost:${PORT}`)
+    console.log(`App is listening on port0 http://localhost:${PORT}`)
 );
+
