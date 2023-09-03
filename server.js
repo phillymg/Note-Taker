c3290441b7a3879fbcc3fc928a9f8b7e5367ac04
Philmg <phillipmguetterman@att.net>
2023-09-03 17:23:21 -0500

    add procfile

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const UUID = require('uuid');
 let dB = require('./db/db.json')
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
